50d7d3b0eabf7170f4da9e45aa95db2df1b0367f
narutoskp <dragonskp@gmail.com>
2016-01-12 23:33:23 -0800

    Initial Commit

---
+++ Test1Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test1Weather
+//
+//  Created by Sawakit on 12/1/59.
+//  Copyright © พ.ศ. 2559 Sawakit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
